010d72b04b8a1ef11bbf8a6138b9e1acfa293d4e
artikov <oybekartikov@icloud.com>
2022-09-29 19:38:07 +0500

    Minor updates

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ import bodyParser from 'body-parser'
 import path from 'path'
 import {fileURLToPath} from 'url';
 
+import connectDB from './server/database/connection.js'
+
+import route from './server/routes/router.js'
+
+
 // setting up the path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,6 +23,9 @@ const PORT = process.env.PORT || 5000
 // log requests (morgan module allows log requests on the console whenever we make requests)
 app.use(morgan('tiny'))
 
+// MONGODB connection
+connectDB()
+
 // parse requests to body-parser
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -29,20 +37,7 @@ app.use('/css', express.static(path.resolve(__dirname, 'assets/css')))
 app.use('/img', express.static(path.resolve(__dirname, 'assets/img')))
 app.use('/js', express.static(path.resolve(__dirname, 'assets/js')))
 
-
-// routers
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
-app.get('/add_number', (req, res) => {
-    res.render('add_number')
-})
-
-app.get('/update_number', (req, res) => {
-    res.render('update_number')
-})
-
-
+// load routers
+app.use('/', route)
 
 app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`))
